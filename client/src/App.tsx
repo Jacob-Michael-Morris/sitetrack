919dc0e886e5567f13329ecb507e4ba795f06341
Jacob-Michael-Morris <JacobMichaelMorris@gmail.com>
2026-08-20 15:58:31 -0600

    Added basic react frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>SiteTrack</h1>
+      <p>Jobsite Tool and Maintenance Management System</p>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/Tools.tsx
@@ -0,0 +1,10 @@
+function Tools() {
+  return (
+    <div>
+      <h1>Tools</h1>
+      <p>SiteTrack tool management</p>
+    </div>
+  )
+}
+
+export default Tools
